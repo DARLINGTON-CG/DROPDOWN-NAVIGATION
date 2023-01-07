b00e1745e31cd20ad3391ef269f04a12b6f6efc6
Darlington <yeboahdarlingtonk@gmail.com>
2023-01-07 16:33:25 +0100

    desired spacing achieved

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const primaryNav = document.querySelector(".primary-navigation");
 
 
 navToggle.addEventListener('click', ()=> {
-
     primaryNav.hasAttribute('data-visible') ? navToggle.setAttribute("aria-expanded",false) : navToggle.setAttribute("aria-expanded",true);  
     primaryNav.toggleAttribute('data-visible');
 })
